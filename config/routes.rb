355529a5b32a01e6ffec6399011ec4b7fcd4a609
taskctk <task.kato.0406@gmail.com>
2020-04-27 14:04:26 +0000

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   root 'static_pages#home'
@@ -24,4 +28,20 @@ Rails.application.routes.draw do
   # delete "/users/:id" => "users#destroy" #一覧画面で選択されたデータを削除処理
   
   resources :account_activations, only:[:edit]
+  # get  "/account_activation/<token>/edit" edit  edit_account_activation_url(token)
+  
+  #検索キー　string    digest              authentication
+  #email      password   password_digest    authenticate(password)
+  #id       remember_token   remember_digest    authenticated?(:remember, token)
+  #email      activation_token   activation_digest    authenticated?(:activation, token)
+  #email      reset_token   reset_digest    authenticate(:reset, token)
+  
+  
+  
+  resources :password_resets,  only:[:new, :create, :edit, :update]
+  # get  "/password_resets/new"  new    new_password_reset_path
+  # post  "/password_resets"  creates    password_resets_path
+  # get  "/password_resets/<token>/edit"  edit    edit_password_reset_url(token)
+  # patch  "/password_resets/new"  update   password_reset_url(token)
+     
 end
